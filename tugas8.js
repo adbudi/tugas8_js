634543cfba53bb9d3b09fe3cb99954b33273d796
as <adbudi1@gmail.com>
2021-07-11 17:45:31 +0700

    tugas8 js
    tugas ini di kumpulkan untuk niomic

---
+++ tugas8.js
@@ -0,0 +1,11 @@
+function tampilObject(){
+  var rumah ={
+    type : 'sederhana',
+    harga : '5000000',
+    luas : '200 m3'
+  }
+  rumah.luas=120;
+  rumah.sertifikat ='HGB';
+  console.log(rumah);
+}
+tampilObject()
